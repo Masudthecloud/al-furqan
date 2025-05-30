ea6067984f8ff550a3f806f9ee7c1fecc1004cff
MasudDevSecOps <rahmanmdm16@gmail.com>
2025-05-29 20:24:57 -0500

    Added PWA feat

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,18 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
